d935d47677fb4be45baf6f6b49214982ddff2e5f
dowoniscool <dowoniscool@yahoo.com>
2020-12-27 20:59:06 +0100

    Initial Commit

---
+++ uitest/uitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  uitestApp.swift
+//  uitest
+//
+//  Created by dowoniscool on 27/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct uitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
