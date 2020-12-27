22a138b48624d54f8901fb7a7e97b93bae3210a1
dowoniscool <dowoniscool@yahoo.com>
2020-12-27 23:12:50 +0100

    //In this lesson, I Learned how to use the text element //and how to modify its look and behaviour using modifiers. //In addition to that, I also learned that the UI is //actually generated from Swift Xcode. //Purpose of a modifier : Changes the look and behaviour of a view element.

--- uitest/ContentView.swift
+++ uitest/ContentView.swift
@@ -9,8 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+            Text("Hello from 2020!")
+                // These are the modifiers for the green background and white text
+                .font(.title2)
+                .fontWeight(.semibold)
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                .background(Color.green)
+                .cornerRadius(10)
+                .foregroundColor(Color.white)
+                // These are the additional modifiers for the blue background
+                
+                .padding()
+                .background(Color.blue)
+                .cornerRadius(10)
+        
+                //Try removing each modifier and see what that does to the Text element! (If you see the "resume" button in the upper right corner of your canvas, click it to ensure that your preview canvas updates to code changes!)
+                
+
+        
     }
 }
 
@@ -19,3 +36,9 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+//In this lesson, I Learned how to use the text element
+//and how to modify its look and behaviour using modifiers.
+//In addition to that, I also learned that the UI is
+//actually generated from Swift Xcode.
+//Purpose of a modifier : Changes the look and behaviour of a view element.
