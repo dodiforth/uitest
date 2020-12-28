7389ee77b437441595937688e78c1293b64dd4e7
dowoniscool <dowoniscool@yahoo.com>
2020-12-28 22:06:32 +0100

    Lesson 4 Challenge Bonus Round *added London Section

--- uitest/ContentView.swift
+++ uitest/ContentView.swift
@@ -10,20 +10,51 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        ZStack {
-            
-            Image("toronto")
-            
-            
-            VStack{
-                Text("CN Tower").font(.title).foregroundColor(.white)
-                Text("Toronto").font(.footnote).foregroundColor(.white)
+        VStack {
+            Spacer()
+            ZStack {
+                
+                Image("toronto")
+                    .resizable()
+                    .cornerRadius(8.0)
+                
+                
+                VStack{
+                    Text("CN Tower").font(.title).foregroundColor(.white)
+                    Text("Toronto").font(.footnote).foregroundColor(.white)
+                }
+                .padding()
+                .background(Color.black)
+                .cornerRadius(8.0)
+                .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+                
+                
             }
             .padding()
-            .background(Color.black)
-            .cornerRadius(8.0)
+            Spacer()
             
             
+            ZStack {
+                
+                Image("london")
+                    .resizable()
+                    .cornerRadius(8.0)
+                
+                
+                VStack{
+                    Text("Big Ben").font(.title).foregroundColor(.white)
+                    Text("London").font(.footnote).foregroundColor(.white)
+                }
+                .padding()
+                .background(Color.black)
+                .cornerRadius(8.0)
+                .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+                
+                
+            }
+            .padding()
+            Spacer()
+
         }
         
         
